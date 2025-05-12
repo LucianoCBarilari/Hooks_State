8774fb98f2656ecb3a97fce6b961fde828810b2d
Luciano Castillo Barilari <luciano.castillo.barilari@gmail.com>
2025-05-12 14:22:40 -0300

    Agregar componente FormBasicExample y estilos de Bootstrap

--- hookstate/src/App.tsx
+++ hookstate/src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import FormBasicExample from './components/FormBasicExample';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <FormBasicExample />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
